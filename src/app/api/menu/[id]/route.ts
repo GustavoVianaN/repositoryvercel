1dc3cc5c5d292662a8e74a7c81b39949884646a8
GustavoVianaN <gustavoviana2006@hotmail.com>
2025-07-16 23:48:54 -0300

    ok

--- src/app/api/menu/[id]/route.ts
+++ src/app/api/menu/[id]/route.ts
@@ -1,11 +1,19 @@
+import { NextRequest } from 'next/server';
 import { menu } from '../../../data/data';
 
 export async function GET(
-  request: Request,
+  request: NextRequest,
   { params }: { params: { id: string } }
 ) {
   const food = menu.find(
     (item: { id: number }) => item.id.toString() === params.id
   );
+
+  if (!food) {
+    return new Response(JSON.stringify({ error: 'Item não encontrado' }), {
+      status: 404,
+    });
+  }
+
   return Response.json(food);
 }
