92a398b4b2283fe08ab44440e6767946088a07ea
GustavoVianaN <gustavoviana2006@hotmail.com>
2025-07-17 01:38:29 -0300

    ok

--- src/app/sections/Footer.tsx
+++ src/app/sections/Footer.tsx
@@ -2,108 +2,109 @@
 import React from 'react';
 import './footer.css';
 import SectionFadeIn from '../components/SectionFadeIn';
+
 export default function Footer() {
   return (
     <footer id="footer">
       <div className="footer-top">
         <div className="container">
-          <SectionFadeIn>          <div className="row">
-            <div className="col-lg-3 col-md-6">
-              <div className="footer-info">
-                <h3>Advocacia</h3>
-                <p>
-                  100 Your Street <br />
-                  State 1234, AUS
-                  <br />
-                  <br />
-                  <strong>Phone:</strong> +61 1234 5678
-                  <br />
-                  <strong>Email:</strong> info@example.com
-                  <br />
-                </p>
-                <div className="social-links mt-3">
-                  <a href="#" className="twitter">
-                    <i className="bi bi-twitter-x"></i>
-                  </a>
-                  <a href="#" className="facebook">
-                    <i className="bi bi-facebook"></i>
-                  </a>
-                  <a href="#" className="instagram">
-                    <i className="bi bi-instagram"></i>
-                  </a>
-                  <a href="#" className="youtube">
-                    <i className="bi bi-youtube"></i>
-                  </a>
-                  <a href="#" className="reddit">
-                    <i className="bi bi-reddit"></i>
-                  </a>
+          <SectionFadeIn>
+            <div className="row">
+              <div className="col-lg-3 col-md-6">
+                <div className="footer-info">
+                  <h3>Advocacia</h3>
+                  <p>
+                    Rua Exemplo, 100 <br />
+                    Bairro Centro, Cidade - Estado <br />
+                    CEP 12345-678 <br />
+                    <br />
+                    <strong>Telefone:</strong> +55 (11) 1234-5678 <br />
+                    <strong>Email:</strong> contato@advocacia.com.br <br />
+                  </p>
+                  <div className="social-links mt-3">
+                    <a href="#" className="twitter">
+                      <i className="bi bi-twitter"></i>
+                    </a>
+                    <a href="#" className="facebook">
+                      <i className="bi bi-facebook"></i>
+                    </a>
+                    <a href="#" className="instagram">
+                      <i className="bi bi-instagram"></i>
+                    </a>
+                    <a href="#" className="youtube">
+                      <i className="bi bi-youtube"></i>
+                    </a>
+                    <a href="#" className="reddit">
+                      <i className="bi bi-reddit"></i>
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div className="col-lg-2 col-md-6 footer-links">
-              <h4>Useful Links</h4>
-              <ul>
-                <li>
-                  <i className="bx bx-chevron-right"></i>
-                  <a href="#">Home</a>
-                </li>
-                <li>
-                  <i className="bx bx-chevron-right"></i>
-                  <a href="#">About us</a>
-                </li>
-                <li>
-                  <i className="bx bx-chevron-right"></i>
-                  <a href="#">Menu</a>
-                </li>
-                <li>
-                  <i className="bx bx-chevron-right"></i>
-                  <a href="#">Events</a>
-                </li>
-                <li>
-                  <i className="bx bx-chevron-right"></i>
-                  <a href="#">Gallery</a>
-                </li>
-              </ul>
-            </div>
+              <div className="col-lg-2 col-md-6 footer-links">
+                <h4>Links Úteis</h4>
+                <ul>
+                  <li>
+                    <i className="bx bx-chevron-right"></i>
+                    <a href="#">Início</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right"></i>
+                    <a href="#">Sobre Nós</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right"></i>
+                    <a href="#">Serviços</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right"></i>
+                    <a href="#">Eventos</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right"></i>
+                    <a href="#">Galeria</a>
+                  </li>
+                </ul>
+              </div>
 
-            <div className="col-lg-3 col-md-6 footer-links">
-              <h4>Our Services</h4>
-              <ul>
-                <li>
-                  <i className="bx bx-chevron-right"></i>
-                  <a href="#">Functions</a>
-                </li>
-                <li>
-                  <i className="bx bx-chevron-right"></i>
-                  <a href="#">Parties</a>
-                </li>
-                <li>
-                  <i className="bx bx-chevron-right"></i>
-                  <a href="#">Events</a>
-                </li>
-                <li>
-                  <i className="bx bx-chevron-right"></i>
-                  <a href="#">Private Chef</a>
-                </li>
-                <li>
-                  <i className="bx bx-chevron-right"></i>
-                  <a href="#">Ceremony</a>
-                </li>
-              </ul>
-            </div>
+              <div className="col-lg-3 col-md-6 footer-links">
+                <h4>Nossos Serviços</h4>
+                <ul>
+                  <li>
+                    <i className="bx bx-chevron-right"></i>
+                    <a href="#">Consultoria Jurídica</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right"></i>
+                    <a href="#">Defesa Trabalhista</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right"></i>
+                    <a href="#">Direito Civil</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right"></i>
+                    <a href="#">Direito Empresarial</a>
+                  </li>
+                  <li>
+                    <i className="bx bx-chevron-right"></i>
+                    <a href="#">Atendimento Personalizado</a>
+                  </li>
+                </ul>
+              </div>
 
-            <div className="col-lg-4 col-md-6 footer-newsletter">
-              <h4>Our Newsletter</h4>
-              <p>
-                Tamen quem nulla quae legam multos aute sint culpa legam nostermagna
-              </p>
-              <form action="" method="post">
-                <input type="email" name="email" />
-                <input type="submit" value="Subscribe" />
-              </form>
+              <div className="col-lg-4 col-md-6 footer-newsletter">
+                <h4>Newsletter</h4>
+                <p>
+                  Inscreva-se para receber as últimas novidades e atualizações jurídicas.
+                </p>
+                <form action="" method="post">
+                  <input type="email" name="email" placeholder="Seu email" />
+                  <input type="submit" value="Assinar" />
+                </form>
+              </div>
             </div>
-          </div></SectionFadeIn>
+          </SectionFadeIn>
         </div>
 
         <div className="container">
@@ -112,10 +113,7 @@ export default function Footer() {
             <strong>
               <span>Advocacia</span>
             </strong>{' '}
-            All Rights Reserved
-          </div>
-          <div className="credits">
-            Designed by <a href="#">DSStudio Technology</a>
+            - Todos os direitos reservados
           </div>
         </div>
       </div>
